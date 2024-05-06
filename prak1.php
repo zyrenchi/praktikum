0188182be1e2705c97f0989a3c8497a74492c4ab
Zyrenchi <ayanokyota@gmail.com>
2024-05-06 12:56:47 +0800

    first commit

---
+++ prak1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRAKTIKUM 1 : DASAR PHP</title>
+</head>
+<body>
+    <?php
+    echo "<h1> INSTITUT BISNIS DAN TEKNOLOGI INDONESIA </h1>";
+    echo "Program PHP Pertamaku<br>";
+ ?>
+</body>
+</html>
---
+++ prak2.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h2>Nilai Mahasiswa : </h2>";
+$nim = '2301010367';
+$nama = 'Frizy Yuda Ardika';
+$prodi = 'TI-MDI';
+$nilai = 90;
+echo "NIM : 2301010367 ", $nim, "<br>";
+echo "Nama : Frizy Yuda Ardika", $nama, "<br>";
+echo "Prodi : TI-MDI", $prodi, "<br>";
+echo "Nilai : 90 $nilai";
+?>
